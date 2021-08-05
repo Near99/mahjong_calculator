045ba961e270ca9c1fe00ec40aeef0fb71c6b887
Near99 <wkx0823@gmail.com>
2021-08-06 02:51:42 +0800

    add comments, simple handle if score calculation encounter errors, and add rule fan less than 8 is not allowed to win

--- script.js
+++ script.js
@@ -32,7 +32,18 @@ const storeData = {
 };
 
 const game = {
+  // used for calculating win rate and displaying game round of course
   roundCounter: 0,
+  /**
+   * initialize player infos
+   *
+   * id: unique identifier
+   * name: player name
+   * win: total winning times
+   * winRate: win rate
+   * self: number of times for self draw win
+   * disCardGiven: number of times for given a discard win
+   **/
   playerInfo: [
     {
       id: "p1",
@@ -72,31 +83,37 @@ const game = {
     },
   ],
 
+  // Throw error if all four players score added not equal to zero.
   errorChecking() {
-    // Throw error if all four players score added not equal to zero.
     const res = this.playerInfo.reduce((accu, player) => {
       return accu + player.score;
     }, 0);
     if (res) {
       alert(`Something went wrong! ${res}`);
+      return false;
     } else {
       console.log("no error found");
+      return true;
     }
   },
 
+  // sort playerInfo array in descending order, compute win rates and call errorChecking function
   sorting() {
     // update round
     this.roundCounter++;
     // sorting
-    this.playerInfo.sort((a, b) => (a.score > b.score ? -1 : 1));
+    this.playerInfo.sort((a, b) => b.score - a.score);
     // compute win rate
     this.playerInfo.forEach((player) => {
       player.winRate = Math.floor((player.win / this.roundCounter) * 100);
     });
-    this.errorChecking();
-    storeData.setData();
+    // if no error occurs, store data
+    // the data will render anyways if error ever occurs, reload the page will render prior stored data
+    // it's unlikely to have score computing errors. No extra actions for draw back I think
+    if (this.errorChecking()) storeData.setData();
   },
 
+  // calculating for self draw
   selfDraw(winner, fan) {
     this.playerInfo.forEach((player) => {
       if (player.id === winner) {
@@ -110,6 +127,7 @@ const game = {
     this.sorting();
   },
 
+  // calculating for winning from player's discard
   fromDiscard(winner, loser, fan) {
     this.playerInfo.forEach((player) => {
       if (player.id === winner) {
@@ -127,15 +145,15 @@ const game = {
     this.sorting();
   },
 
+  // winning method controller
   computeScore(winner, loser, fan, isSelfDraw = false) {
-    if (isSelfDraw) {
-      this.selfDraw(winner, fan);
-    } else {
-      this.fromDiscard(winner, loser, fan);
-    }
+    isSelfDraw
+      ? this.selfDraw(winner, fan)
+      : this.fromDiscard(winner, loser, fan);
   },
 };
 
+// render data to screen
 const display = {
   playerRanking: document.querySelector(".ranking"),
   tableBody: document.querySelector(".tableBody"),
@@ -181,28 +199,26 @@ const winDeclaration = (() => {
     const winner = Object.values(winForm)[2].value;
     const loser = Object.values(winForm)[3].value;
     const fan = Number(Object.values(winForm)[4].value);
+    // not allowed to declare a win if fan is less than 8;
+    if (fan < 8) return;
     // prevent both self and from discard are checked at the same time.
     if (self === discard) return;
     // prevent if winner and loser are selected the same, or winner is unselected.
     if (winner === loser || winner === "unselected") return;
-    if (self) {
-      game.computeScore(winner, undefined, fan, true);
-    }
+    // call computeScore accordingly if passes prior checkings;
+    if (self) game.computeScore(winner, undefined, fan, true);
     if (discard) {
       // prevent loser not seleted if win method is from discard
       if (loser === "self") return;
       game.computeScore(winner, loser, fan);
     }
-    // set checkboxes to be unchecked
+    // reset all values to default to prevent miss toucing
     Object.values(winForm)[0].checked = false;
     Object.values(winForm)[1].checked = false;
-    // set winner to unselected
     Object.values(winForm)[2].value = "unselected";
-    // set loser selector to self
     Object.values(winForm)[3].value = "self";
-    // prevent submit the same result for more than once
     Object.values(winForm)[4].value = "";
-    // render data
+    // update screen
     display.renderData();
   });
 })();
@@ -214,6 +230,7 @@ const reset = (() => {
   });
 })();
 
+// load stored data and render to screen when page load
 window.addEventListener("DOMContentLoaded", () => {
   storeData.getData();
   display.renderData();
